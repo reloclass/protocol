4fb627a9d79db06bf8d66465332b4ab71ef13fb6
张文山 <willzhang@hanyunwangxue.com>
2022-01-11 17:56:08 +0800

    【Protocol】0.1.33

---
+++ lesson/update.go
@@ -0,0 +1,19 @@
+package lesson
+
+import (
+	`strconv`
+)
+
+func (u *UpdateReq) ParseAssistantIds() (results []int64, err error) {
+	results = make([]int64, len(u.AssistantIds))
+	for i, id := range u.AssistantIds {
+		var val int64
+		if val, err = strconv.ParseInt(id, 10, 64); nil != err {
+			return
+		}
+
+		results[i] = val
+	}
+
+	return
+}
